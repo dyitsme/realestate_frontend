2679689f0171dee5bb9de24552809b51c6fc5889
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-04-15 22:34:47 +0800

    Added marker icon and modified readme

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,25 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import map from '../styles/map.module.css'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+const icon = L.icon({ 
+  iconUrl: '/icons/marker-icon.png', 
+  shadowUrl: '/icons/marker-shadow.png' 
+})
+
+const pos = {
+  lat: 14.6091, 
+  lng: 121.0223
+}
 
 const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[pos.lat, pos.lng]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[pos.lat, pos.lng]} icon={icon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
