30090afdb72a845e55b267b991fe587d4c64d6f2
Thalyonn <thalyonns@gmail.com>
2024-05-11 16:29:06 +0800

    Update BarChart.tsx
    Removed Outline, and updated colors

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -7,7 +7,23 @@ export const options = {
   indexAxis: 'y' as const,
   elements: {
     bar: {
-      borderWidth: 2,
+      borderWidth: 0,
+	  backgroundColor: (context) => {
+				const chart = context.chart;
+				const { ctx, chartArea } = chart;
+				if(!chartArea) {
+					return null
+				}
+				return getGradientBackground(chart);
+			},
+		borderColor: (context) => {
+				const chart = context.chart;
+				const { ctx, chartArea } = chart;
+				if(!chartArea) {
+					return null
+				}
+				return getGradientBorder(chart);
+			},	
     },
   },
   responsive: true,
@@ -50,7 +66,7 @@ const BarChart = () => {
               label: 'Revenue', 
               data: [0.95, 0.798,  0.792,-0.4, -0.39, -0.31]
             }
-			
+
           ]
         }}
       >
@@ -87,15 +103,28 @@ const BarChart = () => {
   )
 }
 */
-function getGradient(chart)
+function getGradientBackground(chart)
 {
 	const {ctx, chartArea: {top, bottom, left, right}, scales: {x, y} } = chart;
 	const gradientSegment = ctx.createLinearGradient(0 ,bottom, 0, top);
 	
-	gradientSegment.addColorStop(0, 'red');
-	gradientSegment.addColorStop(0.5, 'red');
-	gradientSegment.addColorStop(0.5, 'green');
-	gradientSegment.addColorStop(1, 'green');
+	gradientSegment.addColorStop(0, '#F87171');
+	gradientSegment.addColorStop(0.5, '#F87171');
+	gradientSegment.addColorStop(0.5, '#4ADE80');
+	gradientSegment.addColorStop(1, '#4ADE80');
+	return gradientSegment;
+	
+}
+
+function getGradientBorder(chart)
+{
+	const {ctx, chartArea: {top, bottom, left, right}, scales: {x, y} } = chart;
+	const gradientSegment = ctx.createLinearGradient(0 ,bottom, 0, top);
+	
+	gradientSegment.addColorStop(0, '#FECACA');
+	gradientSegment.addColorStop(0.5, '#FECACA');
+	gradientSegment.addColorStop(0.5, '#BBF7D0');
+	gradientSegment.addColorStop(1, '#BBF7D0');
 	return gradientSegment;
 	
 }
