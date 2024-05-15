265736aa6ec712fd1aad29607206cffc4bf40ce9
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-05-15 18:40:49 +0800

    Made upload svg into separate file to remove warnings

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -11,6 +11,7 @@ import L from 'leaflet'
 import { useEffect } from 'react'
 import { Data } from '@/utils/Data'
 import Searchbar from '@/components/Searchbar'
+import Image from 'next/image'
 
 export default function MyPage() {
   const Map = useMemo(() => dynamic(
@@ -115,9 +116,12 @@ export default function MyPage() {
               <div className="flex items-center justify-center w-full">
               <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-40 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                        {/* <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                        </svg>
+                        </svg> */}
+                        <div className="mb-4">
+                          <Image src="/upload.svg" width={40} height={40} alt="upload"></Image>
+                        </div>
                         <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                         <p className="text-xs text-gray-500">PNG or JPG (MAX SIZE 5MB)</p>
                     </div>
